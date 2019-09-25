9b5d6202dc8dc90465f46d3680395f6a875a12b2
MatiasVernola <54124501+MatiasVernola@users.noreply.github.com>
2019-09-25 20:49:23 -0300

    Subimos el facturar.php

---
+++ Vernola.Matias/Vernola.Matias/hacerLogin.php
@@ -0,0 +1,27 @@
+<?php
+
+$CheckearNombre=$_GET['nombre'];
+$CheckearClave=$_GET['clave'];
+
+$archivo=fopen('usuarios.txt', 'r');
+
+    while(!feof($archivo)) 
+    {
+		$objeto=json_decode(fgets($archivo));
+  		if ($CheckearNombre==$objeto->'nombre' &&  /*Este if es para checkearSiEstaEl usuario en usuarios.txt */
+  			$CheckearClave==$objeto->'clave')
+  		{
+  			header ("Location:ok.php"); /*si esta lo redirecciona a la pagina ok.php*/
+  			fclose($archivo);
+  			exit()
+  		}
+	    else 
+	    {
+	    	header("Location: no.php");
+	    	fclose($archivo);
+	    	exit()
+	    }
+	 }
+	fclose($archivo);
+
+?> 
