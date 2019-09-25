c4497767681c5b9d41d046a86ef0022180db8922
MatiasVernola <54124501+MatiasVernola@users.noreply.github.com>
2019-09-25 20:38:40 -0300

    Primer TP Parcial

---
+++ Vernola.Matias/ok.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="hacerLogin.php">
+ <img src="ok.jpg" borde="0" height="500" width="500"> 
+
+</form> 
+<p>Fuiste registrado correctamente :)</p>
+</body>
+</html>
